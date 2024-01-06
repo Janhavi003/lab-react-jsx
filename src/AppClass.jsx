6a0faabf5dd46be4a20f7bbbb0a1303b39a9d64e
Janhavi003 <janhavi.hivarekar@kalvium.community>
2024-01-06 15:46:00 +0530

    added successfully

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,24 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render(){
+    let data = this.imageData()
+    let styleMyDiv = {
+      display:"grid",
+      gridTemplateColumns:"38vw 38vw",
+      marginLeft:"13vw",
+      rowGap:"2vw"
+    }
+    return(
+      <>
+        <h1 style={{textAlign:"center"}}>Kalvium Gallery</h1>
+        <div style={styleMyDiv}>
+          <img src={data[0].img} alt="" width={"550px"}/>
+          <img src={data[1].img} alt="" width={"550px"}/>
+          <img src={data[2].img} alt="" width={"550px"}/>
+          <img src={data[3].img} alt="" width={"550px"}/>
+        </div>
+      </>
+    )
+  }
 }
